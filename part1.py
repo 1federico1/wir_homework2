287ddb3bf4934b30ff44e129636c2808698bf40b
federico <federico.arcangeli1@gmail.com>
2017-05-15 14:52:59 +0200

    part1: removed normalization by num of nodes

--- part1.py
+++ part1.py
@@ -4,10 +4,9 @@ import csv
 
 path = "datasets/movie_graph.txt"
 # path = "datasets/test_graph.txt"
-result = nx.Graph()
+result = nx.DiGraph()
 alpha = .15
 epsilon = 10 ** -6
-
 
 
 def read_file(path):
@@ -18,6 +17,7 @@ def read_file(path):
     for adjacency_list in graph:
         result.add_node(int(adjacency_list[0]))
         result.add_edge(int(adjacency_list[0]), int(adjacency_list[1]), weight=int(adjacency_list[2]))
+        result.add_edge(int(adjacency_list[1]), int(adjacency_list[0]), weight=int(adjacency_list[2]))
     return result
 
 
@@ -30,31 +30,12 @@ def create_pagerank_vector(graph):
     return page_rank_vector
 
 
-# def norm_weights(graph):
-#     sum = {}
-#     for node_j in graph:
-#         sum[node_j] = 0
-#         for node_i in graph[node_j]:
-#             sum[node_j] += get_weight(graph, node_i, node_j)
-#     result = nx.Graph()
-#
-#     for node_j in graph:
-#         dict_result = {}
-#         for node_i in graph[node_j]:
-#             result.add_node(node_j)
-#             norm = get_weight(graph, node_i, node_j) / sum[node_j]
-#             dict_result[node_i] = norm
-#             result.add_edge(node_j, node_i, weight=dict_result[node_i])
-#             break
-#     return result
-
-
 def pagerank_single_iteration(graph, pagerank_vector):
     next_page_rank_vector = {}
     sum_of_all_partial_values = 0.
     num_nodes = graph.number_of_nodes()
     for node in graph.nodes():
-        next_page_rank_vector[node]= compute_porting_probability(node,graph, next_page_rank_vector, pagerank_vector)
+        next_page_rank_vector[node] = compute_porting_probability(node, graph, next_page_rank_vector, pagerank_vector)
         sum_of_all_partial_values += next_page_rank_vector[node]
 
     fraction_of_leaked_pr_to_give_each_node = compute_teleporting_probability(num_nodes, sum_of_all_partial_values)
@@ -65,25 +46,46 @@ def pagerank_single_iteration(graph, pagerank_vector):
     return next_page_rank_vector
 
 
+def compute_total_weights(graph):
+    total_weights = {}
+    for node in graph.nodes():
+        total = compute_total_weight_for_node(graph, node)
+        total_weights[node] = total
+    return total_weights
+
+
+def normalize_graph(graph):
+    for node in graph:
+        sum_of_weights = 0.
+        for adjacent in graph[node]:
+            sum_of_weights += get_weight(graph, node, adjacent)
+        for adjacent in graph[node]:
+            graph[node][adjacent]['weight'] = get_weight(graph, node, adjacent) / sum_of_weights
+    return graph
+
+
+def compute_porting_probability(node, graph, next_page_rank_vector, pagerank_vector):
+    # total_weight = compute_total_weight_for_node(graph, node)
+    next_page_rank_vector[node] = 0.
+    for node_i in graph[node]:
+        weight = get_weight(graph, node_i, node)
+        next_page_rank_vector[node] += (1 - alpha) * pagerank_vector[node_i] * weight
+
+    return next_page_rank_vector[node]
+
+
 def compute_teleporting_probability(num_nodes, sum_of_all_partial_values):
     leaked_pr = 1. - sum_of_all_partial_values
     fraction_of_leaked_pr_to_give_each_node = leaked_pr / num_nodes
     return fraction_of_leaked_pr_to_give_each_node
 
 
-def compute_porting_probability(node,graph, next_page_rank_vector, pagerank_vector):
+
+def compute_total_weight_for_node(graph, node):
     total_weight = 0.
     for node_i in graph[node]:
         total_weight += get_weight(graph, node, node_i)
-    next_page_rank_vector[node] = 0.
-    for node_i in graph[node]:
-        degree_node_i = graph.degree(node_i)
-        weight = get_weight(graph, node, node_i)
-        weight_norm = weight / total_weight
-        num = (1. - alpha) * (pagerank_vector[node_i]) * weight_norm
-        next_page_rank_vector[node] += num / degree_node_i
-
-    return next_page_rank_vector[node]
+    return total_weight
 
 
 def get_weight(graph, node_j, node_i):
@@ -97,8 +99,7 @@ def compute_distance(vector_1, vector_2):
     return distance
 
 
-def compute_page_rank(path):
-    graph = read_file(path)
+def compute_page_rank(graph):
     previous_page_rank_vector = create_pagerank_vector(graph)
     iterations = 1
     convergence = False
@@ -119,29 +120,13 @@ def compute_page_rank(path):
 if __name__ == '__main__':
     print("computing graph")
     result_graph = read_file(path)
-    # norm_graph = norm_weights(result_graph)
-    # print(norm_graph[1])
-
-    # pagerank_single_iteration(result_graph, pr_vector)
-    # print(result_graph[1])
-    # print("normalizing weights")
-    # normalize_graph(result_graph)
-    # print(result_graph[1])
-    # for key in result_graph:
-    #     for inner_key in result_graph[key]:
-    #         print(result_graph[key][inner_key]['weight'])
-    #         break
-    #     break
-    damping_factor = 1 - alpha
     print("computing page rank vector")
-    # pp.pprint(nx.pagerank(result_graph, alpha=damping_factor, tol=epsilon))
-    mypr = compute_page_rank(path)
-    realpr = nx.pagerank(result_graph, alpha=damping_factor, tol=epsilon)
-    print(compute_distance(mypr, realpr))
-    print ("my page rank:")
-    pp.pprint(mypr)
-#print "real page rank"
-#pp.pprint(realpr)
+    result_graph = normalize_graph(result_graph)
+    mypr = compute_page_rank(result_graph)
+    print("my page rank:")
+
+    for node in sorted(mypr, key=mypr.get, reverse=True):
+        print(node, mypr[node])
 
     totalsum = 0.
     for val in mypr:
