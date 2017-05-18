e8c327fbed36efacba622cb7b82d7f7d4bb11a31
Fabrizio94 <fabriziotropeano@hotmail.it>
2017-05-18 18:03:43 +0200

    part3 : pageranks_values does not work, linear combination to do

--- part3offline.py
+++ part3offline.py
@@ -1,5 +1,4 @@
 import csv
-import pprint as pp
 import part1
 
 category_file = "datasets/category_movies.txt"
@@ -12,8 +11,9 @@ if __name__ == '__main__':
     input_file.close()
     categories = csv.reader(data, delimiter='\t')
     map = {}
+    temp_map = {}
     cont = 1
-    online_input = open('datasets/online_input.txt', 'w')
+    contatore = 1
 
     for category in categories:
         results = []
@@ -21,10 +21,15 @@ if __name__ == '__main__':
         map[cont] = results
         current_graph = graph.subgraph(map[cont])
         current_graph = part1.normalize_graph(current_graph)
-        cont += 1
         teleporting_dist = part1.compute_teleporting_distribution(current_graph)
-        pr_vector = part1.compute_page_rank(current_graph, teleporting_dist)
-        for node in pr_vector:
-            online_input.write(str(node) + "\t{0}".format(pr_vector[node]) + "\t")
-        online_input.write("\n")
-    online_input.close()
+        temp_map[cont] = part1.compute_page_rank(current_graph, teleporting_dist)
+        cont += 1
+
+
+    for map in temp_map:
+        output_file = open('datasets/input_' + str(contatore) + '.txt', 'w')
+        print temp_map[map]
+        for node in temp_map[map]:
+            output_file.write("{}".format(node) + "\t{}".format(temp_map[map][node]) + "\n")
+        output_file.close()
+        contatore += 1
--- part3online.py
+++ part3online.py
@@ -1,55 +1,81 @@
-category_file = "datasets/category_movies.txt"
 path = "datasets/movie_graph.txt"
 import part1
-
+import pprint as pp
 import csv
 
 
-# def compute_bias():
 
+# def compute_list_of_categories(movie, category_file):
+#     movie_categories = []
+#     for category in category_file:
+#         if str(movie) in category_file[category]:
+#             movie_categories.append(1)
+#         else:
+#             movie_categories.append(0)
+#     return movie_categories
 
-
-def compute_list_of_categories(movie, category_file):
-    movie_categories = []
-    for category in category_file:
-        if str(movie) in category_file[category]:
-            movie_categories.append(1)
+def compute_page_rank_movie(map, movie):
+    list_of_page_ranks = []
+    for vector in map:
+        if(movie in vector):
+            list_of_page_ranks.append(vector[movie])
         else:
-            movie_categories.append(0)
-    return movie_categories
+            list_of_page_ranks.append(0.)
+
+    return list_of_page_ranks
 
 
-def compute_weight(list_of_categories, user_preferences_vector):
-    total = 0.
-    weight = 0.
-    cont = 0
-    for preference in user_preferences_vector:
-        total += preference
-    while cont < len(user_preferences_vector):
-        weight += user_preferences_vector[cont] * list_of_categories[cont]
-        cont += 1
-    return weight / total
+# def compute_weight(list_of_categories, user_preferences_vector):
+#     total = 0.
+#     weight = 0.
+#     cont = 0
+#     for preference in user_preferences_vector:
+#         total += preference
+#     while cont < len(user_preferences_vector):
+#         weight += user_preferences_vector[cont] * list_of_categories[cont]
+#         cont += 1
+#     return weight / total
 
+def from_file_to_map(file):
+    map = {}
+    input_file = open(file, 'r')
+    data = csv.reader(input_file, delimiter = '\t')
+    for line in data:
+        key = int(line[0])
+        map[key] = float(line[1])
+    return map
+
+def pageranks_values(movie, maps):
+    vector = []
+    for map in maps:
+        if movie in map:
+            vector.append(map[movie])
+        else:
+            vector.append(0.)
+    pp.pprint(vector)
+    return vector
 
 
 if __name__ == '__main__':
     user_preferences_vector = [3, 4, 0, 2, 1]
-    input_file = open(category_file, 'r')
-    data = input_file.readlines()
-    input_file.close()
-    categories = csv.reader(data, delimiter='\t')
     graph = part1.read_file(path)
     norm_graph = part1.normalize_graph(graph)
-    map = {}
     cont = 1
+    files = ['datasets/input_1.txt', 'datasets/input_2.txt', 'datasets/input_3.txt', 'datasets/input_4.txt', 'datasets/input_5.txt']
+    maps = {}
 
-    for category in categories:
-        map[cont] = category
+    for file in files:
+        maps[cont] = from_file_to_map(file)
         cont+=1
 
-
     for movie in norm_graph:
-        list_of_categories = compute_list_of_categories(movie, map)
-        weight = compute_weight(list_of_categories, user_preferences_vector)
-        print (movie, weight)
+        movie_pr_vector = pageranks_values(movie, maps)
+        break
+
+
+
+
+    # for movie in norm_graph:
+    #     page_rank_movie_vector = compute_page_rank_movie(map, movie);
+    #     print (movie, weight)
 
